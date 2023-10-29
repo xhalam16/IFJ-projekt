266d781193d690bf5902359428d53d3ba7017f15
Marek Halamka <marek.halamka1@seznam.cz>
2023-10-29 18:07:55 +0100

    finishing scanner, symtable h file

--- src/header_files/scanner.h
+++ src/header_files/scanner.h
@@ -7,8 +7,10 @@
 #include <stdlib.h>
 #include "token.h"
 #include "error.h"
+#include <math.h>
 
 extern bool in_string_global;
+extern bool in_block_comment_global;
 
 typedef struct keyword_type
 {
@@ -37,16 +39,21 @@ const keyword_token_type_t keywords_map[] = {
 
 #define KEYWORD_COUNT sizeof(keywords_map) / sizeof(keywords_map[0])
 
-
+token_type_t keyword_2_token_type(char *keyword);
+bool is_escape_sequence(char c);
+int skip_block_comment(FILE *source_file);
+int skip_line_comment(FILE *source_file);
 bool word_is_keyword(char *word);
-
-bool token_is_keyword(token_t token);
-
+bool word_is_identifier(char *word);
+bool word_is_keyword_datatype(char *word);
 int get_char(FILE *source_file);
 
 void skip_whitespace(FILE *source_file);
 
 token_t get_token(FILE *source_file);
+void unget_token(token_t token, FILE *source_file);
+token_t peek_token(FILE *source_file);
+
 
 
 
