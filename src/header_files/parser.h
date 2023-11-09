112656135ac8b9d5657bdf77abaf8b0f9e858c73
simonmotl <simon.motl@seznam.cz>
2023-11-09 23:20:22 +0100

    parser - funcDeclaration, tree fixed?

--- src/header_files/parser.h
+++ src/header_files/parser.h
@@ -8,23 +8,31 @@
 #include "scanner.h"
 
 FILE *file;
+error_code_t error;
 
 typedef enum NodeType {
     NODE_PROGRAM,
     NODE_ASSIGN,
     NODE_DECLARATION,
+    NODE_DECLARATION_FUNCTION,
     NODE_EXPRESSION,
     NODE_IF_STATEMENT,
     NODE_ELSE_STATEMENT,
     NODE_KEYWORD_LET,
     NODE_IDENTIFIER,
+    NODE_KEYWORD_FUNC,
     NODE_FUNCTION_CALL,
     NODE_FUNCTION_PARAMS,
     NODE_FUNCTION_PARAM,
+    NODE_FUNCTION_PARAM_TYPE,
+    NODE_PARAM_VALUE,
+    NODE_PARAM_LIST,
     NODE_INT,
     NODE_DOUBLE,
     NODE_STRING,
-    NODE_NIL
+    NODE_NIL,
+    NODE_EPSILON,
+    NODE_UNDERSCORE
 } NodeType;
 
 typedef struct TreeNode {
@@ -34,27 +42,9 @@ typedef struct TreeNode {
     unsigned numChildren;
 } TreeNode;
 
-void dispose(TreeNode *node, unsigned numChildren);
+void dispose(TreeNode *parseTree);
 
-TreeNode *createNewNode(TreeNode *node, error_code_t *error);
+TreeNode *createNewNode(TreeNode *parent, NodeType type, bool terminal);
 
-int skipEmptyLines(token_t *token, error_code_t *error);
-
-bool parseDeclaration(TreeNode *neterminal, error_code_t *error);
-
-bool parseCondition(TreeNode *neterminal, error_code_t *error, bool inParenthesis);
-
-bool parseKeyWord(error_code_t *error);
-
-bool parseParameters(TreeNode *node, error_code_t *error);
-
-bool parseAssign(TreeNode *assign, error_code_t *error);
-
-bool parseFuncCall(TreeNode *node, error_code_t *error);
-
-bool parseId(TreeNode *node, error_code_t *error, bool declaration);
-
-bool parseIfStatement(TreeNode *node, error_code_t *error);
-
-bool parse(TreeNode *startNeterminal, error_code_t *error, bool innerBlock);
+bool parse(TreeNode *startNeterminal, bool innerBlock);
 
