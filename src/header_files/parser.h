1afc8c8672057b858d5f9cd31db23c2b5b3e756c
Marek Halamka <135802586+xhalam16@users.noreply.github.com>
2023-11-19 19:40:31 +0100

    Update parser.h

--- src/header_files/parser.h
+++ src/header_files/parser.h
@@ -73,8 +73,7 @@ typedef enum NodeType {
     NODE_DATATYPE_DOUBLE_NILABLE,
     NODE_DATATYPE_STRING_NILABLE,
     NODE_EPSILON,
-    NODE_UNDERSCORE,
-    NODE_SHIFTER
+    NODE_UNDERSCORE
 } NodeType;
 
 typedef enum RuleType {
@@ -86,12 +85,6 @@ typedef enum RuleType {
     RULE_DIV,
     RULE_COALESCING,
     RULE_UNARY,
-    RULE_BELOW,
-    RULE_ABOVE,
-    RULE_BEQ,
-    RULE_AEQ,
-    RULE_EQUAL,
-    RULE_NEQ,
 } RuleType;
 
 typedef struct t_n_mapping {
